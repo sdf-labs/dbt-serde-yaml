4d8c9fbdac4f734364cb1a39e9dc90eb254a649f
Alexander Bogdanowicz <alexkbog@gmail.com>
2025-07-01 02:26:44 -0700

    ...

--- dbt-serde_yaml_schemars_derive/src/schema_exprs.rs
+++ dbt-serde_yaml_schemars_derive/src/schema_exprs.rs
@@ -296,11 +296,12 @@ fn expr_for_adjacent_tagged_enum<'a>(
 
             let set_additional_properties =
                 if let Some(additional_field) = additional_properties_field {
-                    // Use the type of the __additional_properties__ field
-                    let (ty, type_def) = type_for_field_schema(additional_field);
+                    // For BTreeMap<String, T> or HashMap<String, T>, we want to use T as the additional_properties schema
+                    // Extract the value type from Map types
+                    let (value_ty, type_def) = extract_map_value_type(additional_field);
                     let generator = quote!(generator);
-                    let mut schema_expr = quote_spanned! {ty.span()=>
-                        #generator.subschema_for::<#ty>()
+                    let mut schema_expr = quote_spanned! {value_ty.span()=>
+                        #generator.subschema_for::<#value_ty>()
                     };
 
                     prepend_type_def(type_def.clone(), &mut schema_expr);
@@ -428,34 +429,6 @@ fn expr_for_newtype_struct(field: &Field) -> TokenStream {
     expr_for_field(field, true)
 }
 
-/// Extract the value type from Map types like BTreeMap<String, T> or HashMap<String, T>
-/// Returns the T type and any associated type definitions
-fn extract_map_value_type(field: &Field) -> (syn::Type, Option<TokenStream>) {
-    let (full_ty, type_def) = type_for_field_schema(field);
-
-    // Try to extract the value type from common map types
-    if let syn::Type::Path(type_path) = &full_ty {
-        if let Some(segment) = type_path.path.segments.last() {
-            if segment.ident == "BTreeMap" || segment.ident == "HashMap" {
-                if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
-                    if args.args.len() == 2 {
-                        if let (
-                            syn::GenericArgument::Type(_key_ty),
-                            syn::GenericArgument::Type(value_ty),
-                        ) = (&args.args[0], &args.args[1])
-                        {
-                            return (value_ty.clone(), type_def);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Fall back to the full type if we can't extract the value type
-    (full_ty, type_def)
-}
-
 fn expr_for_tuple_struct(fields: &[Field]) -> TokenStream {
     let fields: Vec<_> = fields
         .iter()
@@ -493,6 +466,7 @@ fn expr_for_struct(
     let (flattened_fields, property_fields): (Vec<_>, Vec<_>) =
         filtered_fields.into_iter().partition(|f| f.is_flatten());
 
+    // Look for __additional_properties__ field among flattened fields
     let additional_properties_field = flattened_fields
         .iter()
         .find(|f| f.name() == "__additional_properties__")
@@ -585,7 +559,7 @@ fn expr_for_struct(
         quote! {
             {
                 #type_def
-                object_validation.additional_properties = Some(Box::new(#schema_expr));
+                object_validation.additional_properties = Some(Box::new(#schema_expr)),
             }
         }
     } else if deny_unknown_fields {
@@ -687,4 +661,32 @@ fn prepend_type_def(type_def: Option<TokenStream>, schema_expr: &mut TokenStream
             }
         }
     }
+}
+
+/// Extract the value type from Map types like BTreeMap<String, T> or HashMap<String, T>
+/// Returns the T type and any associated type definitions
+fn extract_map_value_type(field: &Field) -> (syn::Type, Option<TokenStream>) {
+    let (full_ty, type_def) = type_for_field_schema(field);
+
+    // Try to extract the value type from common map types
+    if let syn::Type::Path(type_path) = &full_ty {
+        if let Some(segment) = type_path.path.segments.last() {
+            if segment.ident == "BTreeMap" || segment.ident == "HashMap" {
+                if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                    if args.args.len() == 2 {
+                        if let (
+                            syn::GenericArgument::Type(_key_ty),
+                            syn::GenericArgument::Type(value_ty),
+                        ) = (&args.args[0], &args.args[1])
+                        {
+                            return (value_ty.clone(), type_def);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Fall back to the full type if we can't extract the value type
+    (full_ty, type_def)
 }
